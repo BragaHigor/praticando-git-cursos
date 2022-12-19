9d595ef389ccced12dcb58661d9d74f055b7232f
Higor Braga <higorbraga.s@gmail.com>
2022-12-19 12:30:00 -0300

    Curso

--- 3-JavaScript programando na linguagem da web/01--Aula/js/form.js
+++ 3-JavaScript programando na linguagem da web/01--Aula/js/form.js
@@ -9,14 +9,34 @@ botaoAdicionar.addEventListener('click', function(event){
     //cria a tr a td do paciente
     var pacienteTr = montaTr(paciente);
 
+    var erro = validaPaciente(paciente);
+
+    if (erro.length > 0){
+        exibeMensagemDeErro(erro);
+        return;
+    }
+
     //Adicionando paciente na tabela
     var tabela = document.querySelector('#tabela-pacientes')
 
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagemErro = document.querySelector('#mensagem-erro');
+    mensagemErro.innerHTML = ""
     
 });
+
+function exibeMensagemDeErro(erros){
+    var ul = document.querySelector('#mensagem-erro');
+    ul.innerHTML = ""
+
+    erros.forEach(function(erro){
+        var li = document.createElement('li');
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form){
 
@@ -51,4 +71,36 @@ function montaTd(dado, classe){
     td.classList.add(classe)
 
     return td;
+}
+
+function validaPaciente (paciente){
+
+    var erros = [];
+
+    if (paciente.nome.length == 0){
+        erros.push('O campo "Nome" do paciente não pode ser em branco');
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push('Peso é inválido!');
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push('Altura é inválida!'); 
+    }
+
+    if (paciente.gordura.length == 0){
+        erros.push('O campo "Gordura" do paciente não pode ser em branco');
+    }
+
+    if (paciente.peso.length == 0){
+        erros.push('O campo "Peso" do paciente não pode ser em branco');
+    }
+
+    if (paciente.altura.length == 0){
+        erros.push('O campo "Altura" do paciente não pode ser em branco')
+    }
+
+    return erros;
+
 }
