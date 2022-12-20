aa81b661cffa965ccfad836187530a797cbeb505
Higor Braga <higorbraga.s@gmail.com>
2022-12-19 22:00:31 -0300

    Cursos

---
+++ 1-Curso de JavaScript e TypeScript do basico ao avancado/1--JavaScript/aula 2/index.js
@@ -0,0 +1,14 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// Não utilizar var, utilize const.
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero + segundoNumero;
+
+console.log(resultado);
