eb3f666d7a34120878adf5f11357c9f74977bcab
Higor Braga <higorbraga.s@gmail.com>
2023-01-02 10:10:18 -0300

    TODO

---
+++ 2-React - Matheus Battisti/todo_react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import FirstComponent from './components/FirstComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello React</h2>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2-React - Matheus Battisti/todo_react/src/components/FirstComponents.js
@@ -0,0 +1,11 @@
+function FirstComponent(){
+
+
+    return (
+        <div className="firstcomponents">
+            <p>Primeiro Components</p>
+        </div>
+    )
+}
+
+export default FirstComponent
